75471c18787ce8a946ddbf126cf548338e776d59
libo <1045598742@qq.com>
2020-05-15 13:56:45 +0800

    修复轮播图bug

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,29 @@
     <div id="nav">
       <button @click="show = !show">点击{{value}}</button>
       <lb-date-picker v-model="value"></lb-date-picker>
+      <div style="width:900px;margin:auto" v-if="false">
+          <lb-swiper slide>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/1.jpg" alt="">
+        </lb-swiper-item>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/2.jpg" alt="">
+        </lb-swiper-item>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/3.jpg" alt="">
+        </lb-swiper-item>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/1.jpg" alt="">
+        </lb-swiper-item>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/2.jpg" alt="">
+        </lb-swiper-item>
+        <lb-swiper-item class="demo-item">
+          <img src="../src/assets/3.jpg" alt="">
+        </lb-swiper-item>
+      </lb-swiper>
+      </div>
+    
     </div>
   </div>
 </template>
@@ -16,4 +39,17 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+    .demo-item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .demo-item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
 
